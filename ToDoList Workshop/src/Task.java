44dbcd6fb82728c5e2932817c005341791e0e8c3
dannellim <dannel.lim@premiertaxi.com>
2020-10-16 18:52:47 +0800

    set colorCode to task

--- ToDoList Workshop/src/Task.java
+++ ToDoList Workshop/src/Task.java
@@ -1,8 +1,15 @@
 
 public class Task {
+	
+	private static final int RED = 0;
+	private static final int GREEN = 1;
+	private static final int BLUE = 2;
+	private static final int NONE = 3;
+	
 	private String description = null;
 	private boolean isComplete = false;
 	private boolean isImportant = false;
+	private int colorCode = NONE;
 	
 	public Task(String description) {
 		super();
@@ -22,6 +29,16 @@ public class Task {
 		this.isImportant = isImportant;
 	}
 	
+	
+	
+	public int getColorCode() {
+		return colorCode;
+	}
+
+	public void setColorCode(int colorCode) {
+		this.colorCode = colorCode;
+	}
+
 	public String getDescription() {
 		return description;
 	}
